a09b140a869ecc5fcb9a187dacb92aa16adda83a
elanetto <101054274+elanetto@users.noreply.github.com>
2024-12-10 16:55:50 +0100

    Fixed Tailwind problems with both colors and classes. Also finished the layout for Login, Register and Create Listing pages

--- src/js/utils/fetchLocalData.js
+++ src/js/utils/fetchLocalData.js
@@ -4,31 +4,27 @@ export function fetchLocalData() {
     const avatar = localStorage.getItem('avatar');
     const banner = localStorage.getItem('banner');
 
-    // Update username if the element exists and there's a value
+    // Update username
     const usernameElement = document.querySelector('.username-from-local-storage');
     if (username && usernameElement) {
         usernameElement.textContent = username;
     }
 
-    // Update email if the element exists and there's a value
+    // Update email
     const emailElement = document.querySelector('.email-from-local-storage');
     if (email && emailElement) {
         emailElement.textContent = email;
     }
 
-    // Update avatar if the element exists and there's a value
+    // Update avatar if there's a valid value
     const avatarElement = document.querySelector('.avatar-from-local-storage');
     if (avatar && avatarElement) {
-        avatarElement.style.backgroundImage = `url(${avatar})`;
-        avatarElement.style.backgroundSize = 'cover'; // Optional
-        avatarElement.style.backgroundPosition = 'center'; // Optional
+        avatarElement.src = avatar; // Update the src attribute for the avatar image
     }
 
-    // Update banner if the element exists and there's a value
+    // Update banner if there's a valid value
     const bannerElement = document.querySelector('.banner-from-local-storage');
     if (banner && bannerElement) {
-        bannerElement.style.backgroundImage = `url(${banner})`;
-        bannerElement.style.backgroundSize = 'cover'; // Optional
-        bannerElement.style.backgroundPosition = 'center'; // Optional
+        bannerElement.src = banner; // Update the src attribute for the banner image
     }
 }
